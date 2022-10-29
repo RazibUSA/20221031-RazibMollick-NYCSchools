ed1c2d9aab9d6938271550edcfd36890e82774d5
Razib Mollick <razib.mollick@gmail.com>
2022-10-29 00:19:30 -0500

    Initial Commit

---
+++ 20221031-RazibMollick-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20221031-RazibMollick-NYCSchools
+//
+//  Created by Razib Mollick on 10/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
